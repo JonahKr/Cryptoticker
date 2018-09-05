1cface4d851e5a151f2184a6863d34955b11c49e
unknown <jonah.kraushaar@gmx.net>
2018-09-05 18:32:32 +0200

    debug

--- api.py
+++ api.py
@@ -70,6 +70,7 @@ def getCurrencyPriceById(ccidlist,fclist):
                 price = round(float(data["data"]["quotes"][fc]["price"]), 2)
                 ret_data_sub[fc]=price
             ret_data["\""+id+"\""]=ret_data_sub
+            print ret_data
         return ret_data
     except Exception as e:
         print str(e)
