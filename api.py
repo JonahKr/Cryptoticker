9603d62fa61ec4d0231cf31f40bd176b2ae1f2b1
unknown <jonah.kraushaar@gmx.net>
2018-09-05 16:30:13 +0200

    Updated to V2 API

--- api.py
+++ api.py
@@ -1,46 +1,72 @@
 import requests
-import argparse
 import json
 with open('config.json', 'r') as f:
     config = json.load(f)
 
 # access config
 ccapi_url = config['ccapi_url']
+ccapi_listing_url = config['ccapi_listing_url']
 
-
-parser = argparse.ArgumentParser(description='Crypto Ticker')
-parser.add_argument('-cc', '--cryptocurrency', dest='cryptocurrency',
-                    help='The symbol of the currency you want to be displayed.(standard: BTC)', type=String)
-args = parser.parse_args()
-
-
-def getCurrencyPrice(cc):
-    # https://api.coinmarketcap.com/v1/ticker
+# Check if the symbol really exists
+def getCryptoProve(ccurrencylist):
 	try:
-		symbol = c.upper()
-		apidata = requests.get('ccapi_url')
+		return_cryptourrencylist = []
+		apidata = requests.get(ccapi_listing_url)
 		data = json.loads(apidata.text)
-		for currency in data:
-			if (currency['symbol'] == symbol):
-				price = "{0:.2f}".format(round(float(currency['price_usd']), 2))
-				return price
-		print ("Couldn't find data")
-		return None
+		cryptocurrencylist = data["data"]
+		#go through all cryptocurrencies and look if any of these is similar to one of the passed list
+		for cryptocurrency in cryptocurrencylist:
+			for currency in currencylist:
+				#does the passed symbol equal the cryptocurrency symbol?
+				if(currency.upper()==cryptocurrency['symbol']):
+					return_cryptourrencylist.insert(len(return_cryptourrencylist),currency.upper())
+					print('Existance has been proved.')
+					break
+			#If the ammount of the passed list is reached, he don't has to test any further
+			if(len(ccurrencylist)==len(return_cryptourrencylist)):
+				break
+		return return_cryptourrencylist
 	except Exception as e:
 		print str(e, 'utf-8')
 		return None
 
-def getCurrency24hRate(cc):
+def getCryptoId(ccurrencylist):
 	try:
-		symbol = c.upper()
-		apidata = requests.get('ccapi_url')
+		return_idlist = []
+		apidata = requests.get(ccapi_listing_url)
 		data = json.loads(apidata.text)
-		for currency in data:
-			if (currency['symbol'] == symbol):
-				price = "{0:.2f}".format(round(float(currency['price_usd']), 2))
-				return price
-		print ("Couldn't find data")
+		cryptocurrencylist = data["data"]
+		#go through all cryptocurrencies and look if any of these is similar to one of the passed list
+		for cryptocurrency in cryptocurrencylist:
+			for currency in currencylist:
+				#does the passed symbol equal the cryptocurrency symbol?
+				if(currency.upper()==cryptocurrency['symbol']):
+					return_idlist.insert(len(return_idlist),cryptocurrency["id"])
+					print('ID has been found.')
+					break
+			#If the ammount of the passed list is reached, he don't has to test any further
+			if(len(ccurrencylist)==len(return_idlist)):
+				break
+		return return_idlist
+	except Exception as e:
+		print str(e, 'utf-8')
 		return None
+
+def getCurrencyPriceById(ccidlist,fclist):
+	try:
+		ret_data = {}
+		for id in ccidlist:
+			for fc in fclist:
+				ret_data_sub={}
+				print (ccapi_url+"/"+id+"7"+"?convert="+fc)
+				apidata = request.get(ccapi_url+"/"+id+"7"+"?convert="+fc)
+				data = json.loads(apidata.text)
+				price = "{0:.2f}".format(round(float(data["data"]["quotes"][fc]["price"]), 2))
+				ret_data_sub.insert(fc,price);
+
+			re_data.insert(id,ret_data_sub)
+
+		return ret_data
 	except Exception as e:
 		print str(e, 'utf-8')
 		return None
