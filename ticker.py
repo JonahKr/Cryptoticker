31261bd9f2ab3ef00ff2e858ac601f97b53cf28e
unknown <jonah.kraushaar@gmx.net>
2018-09-06 14:52:09 +0200

    renamed changeget fnkt

--- ticker.py
+++ ticker.py
@@ -23,9 +23,9 @@ class RunText(SampleBase):
         self.fiatcurrencies = api.getFiatProve(fiatcurrencies)
         self.cryptoids = api.getCryptoId(self.cryptocurrencies)
         self.data = api.getCurrencyPriceById(self.cryptoids, self.fiatcurrencies)
-        self.change.h24 = api.get24hChange(self.cryptoids)
-        self.change.h1 = api.get1hChange(self.cryptoids)
-        self.change.d7 = api.get7dChange(self.cryptoids)
+        self.change24h = api.get24hChange(self.cryptoids)
+        self.change1h = api.get1hChange(self.cryptoids)
+        self.change7d = api.get7dChange(self.cryptoids)
 
         print ""
         print " \t\t\t\t~~~~~~~~~~ Setup complete ~~~~~~~~~~ "
